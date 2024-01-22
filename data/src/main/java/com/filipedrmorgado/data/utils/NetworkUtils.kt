d24b9d057b99f8a89fb2a7794e7e2633499e91d0
FilipeMorgado <filipedrmorgado@hotmail.com>
2024-01-22 23:31:10 +0000

    + Added needed Network utils

--- data/src/main/java/com/filipedrmorgado/data/utils/NetworkUtils.kt
+++ data/src/main/java/com/filipedrmorgado/data/utils/NetworkUtils.kt
@@ -1,11 +1,21 @@
 package com.filipedrmorgado.data.utils
 
+import okhttp3.MediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import org.json.JSONObject
+
 // Retrofit strings
 // Lightning Network Calls
 const val active_connection = "Make sure you have an active data connection"
 const val lightning_base_url = "https://legend.lnbits.com/"
 
-const val url_create_wallet = "usermanager/api/v1/users"
+// Account creation requires 2 API requests: account creation and then wallet creation.
+const val url_create_account = "api/v1/account"
+const val url_create_wallet = "/api/v1/wallet"
+
+//const val url_create_wallet = "usermanager/api/v1/users"
 const val url_wallet_balance = "api/v1/wallet"
 const val url_wallet_transactions = "usermanager/api/v1/transactions/"
 const val url_create_check_pay_invoice = "api/v1/payments"
@@ -14,3 +24,12 @@ const val url_import_wallet = "usermanager/api/v1/wallets/"
 
 // HTTP Calls
 const val http_lightning_content_type = "Content-Type: application/json"
+
+// JSON support
+const val json_request_body = "application/json; charset=utf-8"
+
+fun createJsonRequestBody(vararg params: Pair<String, String>): RequestBody {
+    val json = JSONObject(mapOf(*params)).toString()
+    val mediaType: MediaType? = "application/json".toMediaTypeOrNull()
+    return json.toRequestBody(mediaType)
+}
